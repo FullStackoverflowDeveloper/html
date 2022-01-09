423cffe030d8265f96da8fa57b53d0a407bca19e
FullStackoverflowDeveloper <p1973v@yandex.by>
2022-01-09 23:04:23 +0300

    n

---
+++ assets/js/script.js
@@ -0,0 +1,29 @@
+$('.reviews-slider').slick({
+    dots: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+    adaptiveHeight: true
+  });
+
+  $('.mainSlider').slick({
+    dots: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+    adaptiveHeight: true
+  });
+
+
+
+
+var strUp="hello, world!";
+console.log(strUp.toUpperCase());
+
+var strDw="HELLO, WORLD!";
+console.log(strDw.toLowerCase());
+
+
+
+
+
